3200770be71c33dd560f62f4d0930c92a95cb5f6
turmoil2k <juma1112009@hotmail.com>
2022-01-01 04:13:13 +0400

    pushing new gitignore fixes and base files

---
+++ ReminderProject/ReminderProject/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "window.viroos.hhh.ez");
+    sf::CircleShape shapeR(20.f);
+    shapeR.setFillColor(sf::Color::Red);
+    sf::CircleShape shapeG(20.f);
+    shapeG.setPosition(20, 0);
+    shapeG.setFillColor(sf::Color::Green);
+    sf::CircleShape shapeB(20.f);
+    shapeB.setPosition(40, 0);
+    shapeB.setFillColor(sf::Color::Blue);
+
+    bool testWindow = true;
+    if (testWindow)
+    {
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear();
+            window.draw(shapeR);
+            window.draw(shapeG);
+            window.draw(shapeB);
+            window.display();
+        }
+    }
+    else
+    {
+
+    }
+
+	return 0;
+}
