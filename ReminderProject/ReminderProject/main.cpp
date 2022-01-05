9b0f44ea74d24cc139f0e804278e9bb2d6fb87e4
turmoil2k <juma1112009@hotmail.com>
2022-01-05 06:45:38 +0400

    fixed debug issue with loading from file and added basic template of BG & Title

---
+++ ReminderProject/ReminderProject/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    int windowWidth = 1920;
+    int windowHeight = 1080;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "window.viroos.hhh.ez");
+    sf::CircleShape shapeR(20.f);
+    shapeR.setFillColor(sf::Color::Red);
+    sf::CircleShape shapeG(20.f);
+    shapeG.setPosition(20, 0);
+    shapeG.setFillColor(sf::Color::Green);
+    sf::CircleShape shapeB(20.f);
+    shapeB.setPosition(40, 0);
+    shapeB.setFillColor(sf::Color::Blue);
+
+    sf::Font openSansFont;
+    if (!openSansFont.loadFromFile("all/opensans.ttf"))
+    {
+        std::cout << "Fail loading Font!!!" << "\n";
+        // error...
+    }
+    else
+    {
+        std::cout << "working font" << "\n";
+        // working...
+    }
+
+    sf::Text titleText("Welcome to Turmoil's Reminder", openSansFont, 75);
+    titleText.setPosition(windowWidth/5, 10);
+
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("all/bg.jpg"))
+    {
+        std::cout << "Fail loading Texutre!!!" << std::endl;
+    }
+    else
+    {
+        std::cout << "working tex" << "\n";
+        // working...
+    }
+
+    sf::Sprite sprite(backgroundTexture);
+
+    sf::Vector2f targetSize(windowWidth, windowHeight);
+
+    sprite.setScale(
+        targetSize.x / sprite.getLocalBounds().width,
+        targetSize.y / sprite.getLocalBounds().height);
+
+    bool testWindow = true;
+    if (testWindow)
+    {
+        while (window.isOpen())
+        {
+            sf::Event event;
+
+            while (window.pollEvent(event))
+            {
+                // check the type of the event...
+                switch (event.type)
+                {
+                        // window closed
+                    case sf::Event::Closed:
+                        window.close();
+                        break;
+
+                        // key pressed
+                    case sf::Event::KeyPressed:
+                        if (event.key.code == sf::Keyboard::Escape)
+                        {
+                            std::cout << "pressed esc";
+                            window.close();
+                        }
+                        break;
+
+                        // we don't process other types of events
+                    default:
+                        break;
+                }
+            }
+           
+
+            window.clear();
+            window.draw(sprite);
+            window.draw(titleText);
+            window.draw(shapeR);
+            window.draw(shapeG);
+            window.draw(shapeB);
+            window.display();
+        }
+    }
+    else
+    {
+        std::cout << "Testing Window False" << "\n";
+    }
+
+	return 0;
+}
